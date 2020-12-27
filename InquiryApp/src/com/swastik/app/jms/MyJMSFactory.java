e8fdc6749de361c93313b40cf245f36b36007372
Swastik <swastiksuman@gmail.com>
2020-12-27 23:20:16 +0530

    working jms send queue

--- InquiryApp/src/com/swastik/app/jms/MyJMSFactory.java
+++ InquiryApp/src/com/swastik/app/jms/MyJMSFactory.java
@@ -3,20 +3,24 @@ package com.swastik.app.jms;
 import javax.annotation.Resource;
 import javax.jms.JMSException;
 import javax.jms.Queue;
+import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSession;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 public class MyJMSFactory {
-	
-	@Resource(name="jms/EPS_QM")
-	private QueueConnectionFactory qFactory;
-	
-	@Resource(name="jms/EPS_Q")
-	private Queue q;
-	
+
 	public void postAMessage() {
 		try {
-			System.out.println(q.getQueueName());
-		} catch (JMSException e) {
+			Context ctx = new InitialContext();
+			QueueConnectionFactory qConnFactory = (QueueConnectionFactory) ctx.lookup("jms/EPS_QM");
+			QueueConnection queueConnection = qConnFactory.createQueueConnection();
+			QueueSession queueSession = queueConnection.createQueueSession(false, -1);
+			Queue queue = (Queue) ctx.lookup("jms/EPS_Q");
+			System.out.println(queue.getQueueName());
+		} catch (JMSException | NamingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
